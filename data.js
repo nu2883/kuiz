49edd6a6901f7284a4ba3860bd60893d96984e12
nu2883 <39821900+nu2883@users.noreply.github.com>
2024-12-17 10:53:54 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,53 @@
+const soal = [
+    {
+      text: "Siapa presiden pertama Indonesia?",
+      options: [
+        "Soekarno",
+        "Soeharto",
+        "BJ Habibie",
+        "Megawati Soekarnoputri",
+      ],
+    },
+    {
+      text: "Apa planet terbesar dalam tata surya?",
+      options: ["Jupiter", "Saturnus", "Mars", "Bumi"],
+    },
+    {
+      text: "Siapa penulis novel 'Laskar Pelangi'?",
+      options: [
+        "Andrea Hirata",
+        "Tere Liye",
+        "Pramoedya Ananta Toer",
+        "Dewi Lestari",
+      ],
+    },
+    {
+      text: "Apa ibu kota Australia?",
+      options: ["Canberra", "Sydney", "Melbourne", "Brisbane"],
+    },
+    {
+      text: "Siapa penemu bola lampu?",
+      options: ["Thomas Edison", "Nikola Tesla", "Alexander Graham Bell", "James Watt"],
+    },
+    {
+      text: "Dalam sistem tubuh manusia, organ mana yang memompa darah?",
+      options: ["Jantung", "Paru-paru", "Liver", "Ginjal"],
+    },
+    {
+      text: "Apa simbol kimia untuk air?",
+      options: ["H2O", "O2", "CO2", "NaCl"],
+    },
+    {
+      text: "Gunung apa yang tertinggi di dunia?",
+      options: ["Mount Everest", "K2", "Kilimanjaro", "Elbrus"],
+    },
+    {
+      text: "Apa nama benua terbesar di dunia?",
+      options: ["Asia", "Afrika", "Amerika", "Antartika"],
+    },
+    {
+      text: "Siapa tokoh dalam sejarah yang dikenal dengan teori gravitasi?",
+      options: ["Isaac Newton", "Albert Einstein", "Galileo Galilei", "Aristoteles"],
+    },
+  ];
+  
